945ae4b6771862733229a02fb5faff90475f3cd0
Luka <luka.jovanovic.tiger@gmail.com>
2019-08-10 14:05:52 +0200

    SplashActivity update

--- app/src/main/java/com/makina/shows_lukajovanovic/ui/welcome/SplashActivity.kt
+++ app/src/main/java/com/makina/shows_lukajovanovic/ui/welcome/SplashActivity.kt
@@ -5,13 +5,19 @@ import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.os.Bundle
 import android.os.Handler
+import android.renderscript.Sampler
 import android.util.Log
 import android.view.View
 import android.view.animation.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
 import androidx.core.view.doOnLayout
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
 import com.makina.shows_lukajovanovic.R
+import com.makina.shows_lukajovanovic.data.network.ResponseStatus
+import com.makina.shows_lukajovanovic.ui.MainContainerActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
 
@@ -20,63 +26,79 @@ class SplashActivity: AppCompatActivity() {
 	companion object {
 		const val START_ACTIVITY_CODE = 1
 	}
+	private var active = true
+
 ///TODO resolve-aj PR za 7. zadacu (netowrking)
+
+	private lateinit var viewModel: LoginViewModel
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		active = true
 		setContentView(R.layout.activity_splash)
-
-		//TODO zasto se animacija nece pokrenuti ako je view na pocetku bio izvan ekrana?
-		//TODO zasto animacija nece raditi ako je samo u onCreate odmah pokrenuta, a ne u layout.doOnLayout?
-		//TODO podesi tocnu granicu
+		viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+		viewModel.tokenResponseLiveData.observe(this, Observer {})
 		textViewTitle.visibility = View.INVISIBLE
 		layout.doOnLayout {
-			imageViewLogo.animate()
-				.y(layout.height.toFloat() / 2 - imageViewLogo.height)
-				.setInterpolator(BounceInterpolator())
-				.setDuration(1000)
-				.setStartDelay(500)
-				.setListener(object : Animator.AnimatorListener {
-					override fun onAnimationEnd(p0: Animator?) {
-						animateTitle()
-					}
-
-					override fun onAnimationRepeat(p0: Animator?) {}
-					override fun onAnimationCancel(p0: Animator?) {}
-					override fun onAnimationStart(p0: Animator?) {}
-
-				})
-				.start()
+			animateLogo()
 		}
-
 	}
 
 	private val handlerThread = Handler()
 	private val startActivityRunnable = Runnable {
-		startActivity(LoginActivity.newInstance(this))
-	}
-
-	fun animateTitle() {
-		val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
-		valueAnimator.addUpdateListener {
-			val value = it.animatedValue as Float
-			textViewTitle.visibility = View.VISIBLE
-			textViewTitle.scaleX = value
-			textViewTitle.scaleY = value
+		if(viewModel.tokenResponse?.status == ResponseStatus.SUCCESS && (viewModel.tokenResponse?.token ?: "") != "") {
+			startActivity(MainContainerActivity.newInstance(this))
 		}
-
-		valueAnimator.interpolator = OvershootInterpolator()
-		valueAnimator.duration = 500
-		valueAnimator.doOnEnd {
-			//TODO ne smijem dodati ako je activity ugasen
-			handlerThread.postDelayed(startActivityRunnable, 2000)
+		else {
+			startActivity(LoginActivity.newInstance(this))
 		}
-		valueAnimator.start()
-	}
-
-	override fun onStop() {
-		handlerThread.removeCallbacks(startActivityRunnable)
-		super.onStop()
 		finish()
 	}
 
+	private lateinit var valueAnimatorLogo: ValueAnimator
+	private fun animateLogo() {
+		valueAnimatorLogo = ValueAnimator.ofFloat(imageViewLogo.y, guidelineCenter.y - imageViewLogo.height)
+		with(valueAnimatorLogo) {
+			addUpdateListener {
+				imageViewLogo.y = it.animatedValue as Float
+			}
+			interpolator = BounceInterpolator()
+			duration = 1000
+			doOnEnd {
+				if(active) animateTitle()
+			}
+			startDelay = 500
+			start()
+		}
+	}
+
+
+	private lateinit var valueAnimatorTitle: ValueAnimator
+	private fun animateTitle() {
+		valueAnimatorTitle = ValueAnimator.ofFloat(0f, 1f)
+		with(valueAnimatorTitle) {
+			addUpdateListener {
+				val value = it.animatedValue as Float
+				textViewTitle.visibility = View.VISIBLE
+				textViewTitle.scaleX = value
+				textViewTitle.scaleY = value
+			}
+
+			interpolator = OvershootInterpolator()
+			duration = 500
+			doOnEnd {
+				if (active)
+					handlerThread.postDelayed(startActivityRunnable, 2000)
+			}
+			start()
+		}
+	}
+
+	override fun onStop() {
+		active = false
+		handlerThread.removeCallbacks(startActivityRunnable)
+		if(::valueAnimatorLogo.isInitialized) valueAnimatorLogo.cancel()
+		if(::valueAnimatorTitle.isInitialized) valueAnimatorTitle.cancel()
+		super.onStop()
+	}
 }
